f522f297bcf2181c76dd15506c12185dcf586d11
David <davidginaca@gmail.com>
2023-01-02 17:58:22 -0300

    sin funcionar front y back

---
+++ src/app/servicios/interceptor-service.ts
@@ -0,0 +1,22 @@
+import { HttpEvent, HttpHandler, HttpRequest } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { TokenService } from "./token.service";
+
+export class InterceptorService {
+
+
+    constructor(private tokenService: TokenService){}
+
+    Intercep(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>>{
+        let intReq = req;
+        const token = this.tokenService.getToken();
+        if(token != null){ 
+            intReq= req.clone({
+                headers: req.headers.set('Authorization', 'Bearer' + token)
+            });
+    }
+        return next.handle(intReq);
+    }
+}
+
+export const 
