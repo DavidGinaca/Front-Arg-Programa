1eb12f86db4b2d2516b88981df925d3204dba5a0
David <davidginaca@gmail.com>
2023-01-11 19:55:26 -0300

    peparando para subir

--- src/app/servicios/educacion.service.ts
+++ src/app/servicios/educacion.service.ts
@@ -9,7 +9,7 @@ import { Educacion } from '../model/educacion';
 })
 export class EducacionService {
 
-URL= environment.URL + "personas";
+URL= environment.URL + "educacion/";
   constructor(private httpClient : HttpClient) { }
 
   public lista(): Observable<Educacion[]>{
